52d0144d2801af356afd7fbb8bb1b9372a61f858
Nayabasetsi Kisowile <nkisowile@gmail.com>
2025-06-18 14:35:00 +0300

    chore: add pwsh and zsh dotfiles

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+oh-my-posh init pwsh --config ~\Documents\PowerShell\emodipt-custom\emodipt-custom.omp.json | Invoke-Expression
+
+function rm {
+    param([string]$Path)
+    Remove-Item -Path $Path -Recurse -Force
+}
+
+function c { Clear-Host }
+function x { exit }
+function v { nvim @args }
+function sudo { gsudo @args }
+function ff { fastfetch }
+function nerdfont {
+  & ([scriptblock]::Create((iwr 'https://raw.githubusercontent.com/jpawlowski/nerd-fonts-installer-PS/main/Invoke-NerdFontInstaller.ps1')))
+}
+
+function rm {
+    param([string[]]$Path)
+    Remove-Item -Path $Path -Recurse -Force -ErrorAction Stop
+}
+
+
+Invoke-Expression ((zoxide init powershell) -join "`n")
