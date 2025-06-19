7a653d270ed65abd1704579179a6a4f9974b1786
Nayabasetsi Kisowile <nkisowile@gmail.com>
2025-06-19 11:34:29 +0300

    feat: add lsd aliases for pwsh

--- pwshrc.ps1
+++ pwshrc.ps1
@@ -9,6 +9,10 @@ function c { Clear-Host }
 function x { exit }
 function v { nvim @args }
 function sudo { gsudo @args }
+function l { lsd -l }
+function la { lsd -a }
+function lla { lsd -la }
+function lt { lsd --tree }
 function ff { fastfetch }
 function nerdfont {
   & ([scriptblock]::Create((iwr 'https://raw.githubusercontent.com/jpawlowski/nerd-fonts-installer-PS/main/Invoke-NerdFontInstaller.ps1')))
