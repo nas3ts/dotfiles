1c55521ae86b4a514728b157bda82398a291c7ec
Nayabasetsi J. Kisowile <166422387+nas3ts@users.noreply.github.com>
2025-06-29 23:03:31 +0300

    feat: add gsudo as ensured tool

--- pwshrc.ps1
+++ pwshrc.ps1
@@ -34,6 +34,7 @@ Ensure-Tool -CommandName 'oh-my-posh' -WingetId 'JanDeDobbeleer.OhMyPosh'
 Ensure-Tool -CommandName 'aliae'      -WingetId 'JanDeDobbeleer.aliae'
 Ensure-Tool -CommandName 'zoxide'     -WingetId 'ajeetdsouza.zoxide'
 Ensure-Tool -CommandName 'lsd'	      -WingetId 'lsd-rs.lsd'
+Ensure-Tool -CommandName 'gsudo'      -WingetId 'gerardog.gsudo'
 
 # --- Paths and Inits ---
 
