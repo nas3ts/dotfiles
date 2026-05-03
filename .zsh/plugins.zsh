4af65d4bc9df2c4078d711563669860058681a3f
nas3ts <nkisowile@gmail.com>
2026-05-03 13:42:26 +0300

    Fix zinit errors in plugins.zsh causing exit code 1

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,21 +1,20 @@
 # --- Zsh Plugins ---
 
-zinit light zsh-users/zsh-autosuggestions
-fpath=(~/.zsh/zsh-completions /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/functions/Calendar /usr/share/zsh/functions/Chpwd /usr/share/zsh/functions/Completion /usr/share/zsh/functions/Completion/Base /usr/share/zsh/functions/Completion/Linux /usr/share/zsh/functions/Completion/Unix /usr/share/zsh/functions/Completion/X /usr/share/zsh/functions/Completion/Zsh /usr/share/zsh/functions/Exceptions /usr/share/zsh/functions/MIME /usr/share/zsh/functions/Math /usr/share/zsh/functions/Misc /usr/share/zsh/functions/Newuser /usr/share/zsh/functions/Prompts /usr/share/zsh/functions/TCP /usr/share/zsh/functions/VCS_Info /usr/share/zsh/functions/VCS_Info/Backends /usr/share/zsh/functions/Zftp /usr/share/zsh/functions/Zle)
+if command -v zinit >/dev/null 2>&1; then
+  zinit light zsh-users/zsh-autosuggestions
+  zinit light zsh-users/zsh-syntax-highlighting
 
+  typeset -A ZSH_HIGHLIGHT_STYLES
 
-typeset -A ZSH_HIGHLIGHT_STYLES
-
-ZSH_HIGHLIGHT_STYLES=(
-  'default'             'fg=#C0C0C0'           # Default color for most things (commands, arguments, etc.)
-  'command'             'fg=#E06C75'           # Pinkish red for recognized commands
-  'argument'            'fg=#F3C267'           # Yellow for arguments
-  'option'              'fg=#61AFEF'           # Cyan for options (e.g., -a, --help)
-  'path'                'fg=#58A6FF'           # Softer blue for file paths
-  'number'              'fg=#61AFEF'           # Blue for numbers (same as path)
-  'reserved-word'       'fg=#E06C75'           # Red for reserved words like `if`, `for`, etc.
-  'error'               'fg=#E06C75,bold'      # Bold red for unrecognized input (errors)
-  'unknown-command'     'fg=#E06C75'           # Red for unknown commands
-)
-
-zinit light zsh-users/zsh-syntax-highlighting
+  ZSH_HIGHLIGHT_STYLES=(
+    'default'             'fg=#C0C0C0'
+    'command'             'fg=#E06C75'
+    'argument'            'fg=#F3C267'
+    'option'              'fg=#61AFEF'
+    'path'                'fg=#58A6FF'
+    'number'              'fg=#61AFEF'
+    'reserved-word'       'fg=#E06C75'
+    'error'               'fg=#E06C75,bold'
+    'unknown-command'     'fg=#E06C75'
+  )
+fi
